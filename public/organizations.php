943e867b4bf5834f0c99f5e3f1278cd44ff542b9
superdeveloper910 <diego.romero89910@gmail.com>
2018-09-19 05:48:16 +0200

    datasync

--- public/organizations.php
+++ public/organizations.php
@@ -170,9 +170,20 @@
 								}
 								$services_count= sizeof(explode(",", $services));
 								$tags = implode(",", $record['fields']['tags']);
+								$website = str_replace("https://","",$record['fields']['website']);
+								$website = str_replace("http://","",$website);
+
+								$twitter = str_replace("https://","",$record['fields']['Twitter']);
+								$twitter = str_replace("http://","",$twitter);
+
+								$facebook = str_replace("https://","",$record['fields']['Facebook']);
+								$facebook = str_replace("http://","",$facebook);
+
+								$rss = str_replace("https://","",$record['fields']['RSS']);
+								$rss = str_replace("http://","",$rss);
 
 								$sql = "INSERT INTO organizations (organization_id, organizations_id, alternate_name, name, dedupe, type, child_of, contacts, website, description, logo, checkbook, internalnotes, contacts_link, services, phones, main_address, details, program, email, legal_status, tax_status, tax_id, year_incorporated, Twitter, Facebook, RSS, tags, services_count)
-								VALUES ('{$record['id']}', '{$organizations_id}', '{$alternate_name}', '{$name}', '{$record['fields']['dedupe']}', '{$record['fields']['Type']}', '{$record['fields']['Child of']}', '{$contacts}', '{$website}',  '{$description}', '{$logo}', '{$record['fields']['checkbook']}', '{$internalnotes}', '{$record['fields']['contacts link']}', '{$services}', '{$main_phone}', '{$main_address}', '{$details}', '{$program}', '{$record['fields']['email']}', '{$record['fields']['legal_status']}', '{$record['fields']['tax_status']}', '{$record['fields']['tax_id']}', '{$record['fields']['year_incorporated']}', '{$record['fields']['Twitter']}', '{$record['fields']['Facebook']}', '{$record['fields']['RSS']}', '{$tags}','{$services_count}');";
+								VALUES ('{$record['id']}', '{$organizations_id}', '{$alternate_name}', '{$name}', '{$record['fields']['dedupe']}', '{$record['fields']['Type']}', '{$record['fields']['Child of']}', '{$contacts}', '{$website}',  '{$description}', '{$logo}', '{$record['fields']['checkbook']}', '{$internalnotes}', '{$record['fields']['contacts link']}', '{$services}', '{$main_phone}', '{$main_address}', '{$details}', '{$program}', '{$record['fields']['email']}', '{$record['fields']['legal_status']}', '{$record['fields']['tax_status']}', '{$record['fields']['tax_id']}', '{$record['fields']['year_incorporated']}', '{$twitter}', '{$facebook}', '{$rss}', '{$tags}','{$services_count}');";
 
 								
 

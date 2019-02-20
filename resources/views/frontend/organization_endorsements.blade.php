4c7ae13bee47b05f00c39cfdeb76906e9f23ae7d
Milos <grantlatest@gmail.com>
2019-02-20 21:06:34 +0100

    candidate

--- resources/views/frontend/organization_endorsements.blade.php
+++ resources/views/frontend/organization_endorsements.blade.php
@@ -162,7 +162,7 @@
                                             @foreach($endorsements as $endorsement)
                                             <tr>
                                                 <td style="width: 10%;">@if($endorsement->elections()->first()) <a href="/elections/{{$endorsement->elections()->first()->year}}">{{$endorsement->elections()->first()->name}}</a> @endif</td>
-                                                <td style="text-align: left; width: 25%;"><a href="/organization_{{$organization->organizations_id}}/candidates/{{$endorsement->candidate()->first()->id}}">{{$endorsement->candidate()->first()->name}}</a></td>
+                                                <td style="text-align: left; width: 25%;"><a href="/candidates/{{$endorsement->candidate()->first()->id}}">{{$endorsement->candidate()->first()->name}}</a></td>
                                                 <td style="width: 20%;">{{$endorsement->parties_name}}</td>
                                                 <td style="text-align: left;">@if(isset($endorsement->offices()->first()->organization))<a href="/organization_{{$endorsement->offices()->first()->organizationid}}/candidates"> {{$endorsement->offices()->first()->organization}}</a>@endif</td>
                                                 <td style="width: 15%;">{{$endorsement->date_recorded}}</td>

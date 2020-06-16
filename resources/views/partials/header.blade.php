52d1dcc5ad3bcce024d5cfa252569369aa23ec26
loveisbluenova <alekssoftdev@gmail.com>
2020-06-16 09:25:57 -0400

    added top menu

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,28 +4,50 @@
 
 		</div>
 	</div> -->
-	<div class="mdl-layout-logo mdl-layout__header-row">
-		<div class="container">
-	  	<a class="logo_left" href="/">@if($layout->find(1)->action ==1 ) <p class="d-inline site-name">{{$layout->find(1)->link}}</p> @endif @if($layout->find(3)->action ==1 ) <img src="/upload/images/{{$layout->find(3)->link}}" class="img-responsive d-inline"> @endif @if($layout->find(2)->action ==1 ) <p class="d-inline site-name">{{$layout->find(2)->link}}</p> @endif </a>
-	      <div class="top-bar-right">
-	      	<button type="button" class="btn btn-raised btn-block btn-primary menu_filter">
-		        <i class="fa fa-bars" aria-hidden="true"></i> Menu 
-		    </button>
-			<ul class="register_button">
-				@if (Auth::user())
-				<li>
-					<a href="/auth/logout">Sign out</a>
-				</li>
-				@else
-				<li>
-					<a href="/auth/login">Sign in</a>
-				</li>
-				@endif
-				<li>
-					<a class="button" href="/auth/register">Register</a>
-				</li>
-			</ul>
-	      </div>
+	<div class="logo_area">
+		<div class="container" id="logo_area_container">
+			<div class="border_bottom">
+				<a class="logo_left" href="/">  
+					<img src="/upload/images/1560348477.png" class="img-responsive d-inline">   
+				</a>
+				<!-- start main menu area -->
+				<div class="main_menu">
+					<ul class="external_menubar">
+						<li class="dropdown">
+							<a class="dropdown-toggle menu_link is-active" data-toggle="dropdown" href="javascript:void(0)" aria-expanded="false">
+								<span>Advocacy</span>
+							</a>
+							<ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 43px, 0px);">
+								<li><a href="https://wegov.nyc/211-4-nyc/" class="menu_link">Searchable Safety Net</a></li>
+								<li><a href="https://wegov.nyc/open-engaging-effective-government/" class="menu_link">Digital Transformation</a></li>
+								<li><a href="https://wegov.nyc/nymeta/" class="menu_link">Metro-Regional Leadership</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a class="dropdown-toggle menu_link" data-toggle="dropdown" href="javascript:void(0)" aria-expanded="false"><span>Tools</span></a>
+							<ul class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 43px, 0px);">
+								<li><a href="http://capital.research.wegov.nyc/" class="menu_link">Capital Projects Directory</a></li>
+								<li><a href="http://services.wegov.nyc" class="menu_link">Services Directory &amp; API</a></li>
+								<li><a href="http://nyclaws.readthedocs.io/" class="menu_link">Charter, Code &amp; Rules</a></li>
+								<li><a href="https://maps.wegov.nyc/" class="menu_link">Maps</a></li>
+								<li><a href="https://wegov.nyc/action/" class="menu_link">The Action App!</a></li>
+							</ul>
+						</li>
+						<li><a href="https://wegov.nyc/news/" class="menu_link"><span>Community</span></a></li>
+						<li><a href="https://wegov.nyc/blog/" class="menu_link"><span>Blog</span></a></li>
+						<li><a href="https://opencollective.com/wegovnyc" class="menu_link"><span>Donate</span></a></li>
+						<li><a href="https://wegov.nyc/contact/" class="menu_link"><span>Contact</span></a></li>
+					</ul>
+				</div>
+				<!-- end main menu area -->
+				<!-- <a class="nav-link " id="google_translate_element"></a> -->
+				<div class="top-bar-right">
+					<button type="button" onclick="openNav()" class="btn btn-raised btn-block btn-primary menu_filter">
+						<!-- <img src="resources/menu.svg" alt="" title="">Menu  -->
+						Menu
+					</button>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="responsive_menu">
@@ -37,72 +59,6 @@
 			<a href="http://www.d9ithub.com/nycvoters/app/login.php" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 9) == 'nycvoters') is-active @endif">Voters</a>
 		</div>
 	</div>
-
-	<!-- <div class="responsive_menu">
-		<div class="container">
-			<a href="#" class="mdl-layout__tab menu-link">Sign in</a>
-			<a href="#" class="mdl-layout__tab menu-link">Register</a>
-			<a href="http://wegov.nyc/" class="mdl-layout__tab menu-link">Welcome</a>
-			<a href="http://wegov.nyc/news" class="mdl-layout__tab menu-link">News</a>
-			<a href="http://wegov.nyc/learn/" class="mdl-layout__tab menu-link">Learn</a>
-			<a href="/" class="mdl-layout__tab menu-link  is-active">Research</a>			
-			<div class="tab_filter_btn menu_btn">
-                <ul class="nav nav-tabs nav-menu" role="tablist">
-                    @if($menu->find(1)->action ==1)<li><a href="/organizations" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 12) == 'organization') is-active @endif">Organizations</a></li>@endif
-						@if($menu->find(2)->action ==1)<li><a href="/projects" class="mdl-layout__tab @if(Request::is ('projects')) is-active @endif">Projects</a></li>@endif
-						@if($menu->find(3)->action ==1)<li><a href="/services" class="mdl-layout__tab @if(Request::is ('services')) is-active @endif">Services</a></li> @endif
-						@if($menu->find(4)->action ==1) <li><a href="/people" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 6) == 'people') is-active @endif">People</a></li> @endif
-						@if($menu->find(5)->action ==1) <li><a href="/jobs" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 4) == 'jobs') is-active @endif">Jobs</a></li> @endif
-						@if($menu->find(6)->action ==1) <li><a href="/elections" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 9) == 'elections') is-active @endif">Elections</a></li> @endif
-						@if($menu->find(7)->action ==1) <li><a href="/laws" class="mdl-layout__tab @if(Request::is ('laws')) is-active @endif">Laws</a></li> @endif
-						@if($menu->find(8)->action ==1) <li><a href="/about" class="mdl-layout__tab @if(Request::is ('about')) is-active @endif">About</a></li> @endif
-					
-                </ul>
-            </div>
-            <a href="http://decide.wegov.nyc/legislation/processes" class="mdl-layout__tab menu-link">Decide</a>
-			<a href="http://wegov.nyc/act/" class="mdl-layout__tab menu-link">Act</a>
-			<a href="http://wegov.nyc/about" class="mdl-layout__tab menu-link">About</a>
-			<a href="http://wegov.nyc/blog/" class="mdl-layout__tab menu-link">Blog</a>
-			<a href="https://opencollective.com/wegovnyc" class="mdl-layout__tab menu-link">Donate</a>
-			<a href="http://wegov.nyc/get-involved/" class="mdl-layout__tab menu-link">Get Involved</a>
-			@if($menu->find(1)->action ==1) <a href="/organizations" class="mdl-layout__tab menu-link @if(mb_substr(Request::segment(1), 0, 12) == 'organization') is-active @endif">Organizations</a> @endif
-			@if($menu->find(2)->action ==1) <a href="/projects" class="mdl-layout__tab menu-link @if(Request::is ('projects')) is-active @endif">Projects</a> @endif
-			@if($menu->find(3)->action ==1) <a href="/services" class="mdl-layout__tab menu-link @if(Request::is ('services')) is-active @endif">Services</a> @endif
-			@if($menu->find(4)->action ==1) <a href="/people" class="mdl-layout__tab menu-link @if(mb_substr(Request::segment(1), 0, 6) == 'people') is-active @endif">People</a> @endif
-			@if($menu->find(5)->action ==1) <a href="/jobs" class="mdl-layout__tab menu-link @if(mb_substr(Request::segment(1), 0, 4) == 'jobs') is-active @endif">Jobs</a> @endif
-			@if($menu->find(6)->action ==1) <a href="/elections" class="mdl-layout__tab menu-link @if(mb_substr(Request::segment(1), 0, 9) == 'elections') is-active @endif">Elections</a> @endif
-			@if($menu->find(7)->action ==1) <a href="/laws" class="mdl-layout__tab menu-link @if(Request::is ('laws')) is-active @endif">Laws</a> @endif
-			@if($menu->find(8)->action ==1) <a href="/about" class="mdl-layout__tab menu-link @if(Request::is ('about')) is-active @endif">About</a> @endif
-		</div>
-	</div> -->
-
-	<!-- <div class="mdl-layout__tab-bar mdl-layout__header-row external-menubar ">
-		<div class="container">
-			<a href="http://wegov.nyc/" class="mdl-layout__tab menu-link">Welcome</a>
-			<a href="http://wegov.nyc/news" class="mdl-layout__tab menu-link">News</a>
-			<a href="http://wegov.nyc/learn/" class="mdl-layout__tab menu-link">Learn</a>
-			<a href="/" class="mdl-layout__tab menu-link  is-active">Research</a>
-			<a href="http://decide.wegov.nyc/legislation/processes" class="mdl-layout__tab menu-link">Decide</a>
-			<a href="http://wegov.nyc/act/" class="mdl-layout__tab menu-link">Act</a>
-			<a href="http://wegov.nyc/about" class="mdl-layout__tab menu-link">About</a>
-			<a href="http://wegov.nyc/blog/" class="mdl-layout__tab menu-link">Blog</a>
-			<a href="https://opencollective.com/wegovnyc" class="mdl-layout__tab menu-link">Donate</a>
-			<a href="http://wegov.nyc/get-involved/" class="mdl-layout__tab menu-link">Get Involved</a>
-		</div>
-	</div>
-	<div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark submenu_div">
-		<div class="container">
-			@if($menu->find(1)->action ==1) <a href="/organizations" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 12) == 'organization') is-active @endif">Organizations</a> @endif
-			@if($menu->find(2)->action ==1) <a href="/projects" class="mdl-layout__tab @if(Request::is ('projects')) is-active @endif">Projects</a> @endif
-			@if($menu->find(3)->action ==1) <a href="/services" class="mdl-layout__tab @if(Request::is ('services')) is-active @endif">Services</a> @endif
-			@if($menu->find(4)->action ==1) <a href="/people" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 6) == 'people') is-active @endif">People</a> @endif
-			@if($menu->find(5)->action ==1) <a href="/jobs" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 4) == 'jobs') is-active @endif">Jobs</a> @endif
-			@if($menu->find(6)->action ==1) <a href="/elections" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 9) == 'elections') is-active @endif">Elections</a> @endif
-			@if($menu->find(7)->action ==1) <a href="/laws" class="mdl-layout__tab @if(Request::is ('laws')) is-active @endif">Laws</a> @endif
-			@if($menu->find(8)->action ==1) <a href="/about" class="mdl-layout__tab @if(Request::is ('about')) is-active @endif">About</a> @endif
-		</div>
-	</div> -->
-
 	<div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark submenu_div">
 		<div class="container">
 			<a href="/elections" class="mdl-layout__tab @if(mb_substr(Request::segment(1), 0, 9) == 'elections') is-active @endif">Elections</a>
